4c3b2debd6608f1884d71ed13b1c2758101e195c
Dallamore <25459576+Dallamore@users.noreply.github.com>
2019-01-03 09:15:02 +0000

    Set-up

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+
+function myFunction() {
+  let classValue = document.getElementById("classList").value;
+
+  let myTable= "<table><tr><td>Col Head 1</td>";
+    myTable+= "<td>Col Head 2</td>";
+    myTable+="<td>Col Head 3</td></tr>";
+
+    myTable+="<tr><td>---------------</td>";
+    myTable+="<td>---------------</td>";
+    myTable+="<td>---------------</td></tr>";
+
+
+
+
+
+  document.getElementById("dataSpace").innerHTML = "You selected: " + classValue + myTable;
+}
