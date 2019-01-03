61980cab03696c0758176d31f8d24207f57a80be
Dallamore <25459576+Dallamore@users.noreply.github.com>
2019-01-03 23:00:00 +0000

    Drop down populated from JSON

---
+++ scripts/dndClassData.js
@@ -0,0 +1,53 @@
+let dndClassJSON = `
+{ "bard": [
+  {
+    "name":"Bard" ,
+    "cantrips":"3",
+    "l1":"1"
+  },
+  {
+    "name":"Cleric" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Druid" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Fighter" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Paladin" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Ranger" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Rogue" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Sorcerer" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Warlock" ,
+    "cantrips":"4",
+    "l1":"9"
+  },
+  {
+    "name":"Wizard" ,
+    "cantrips":"1",
+    "l1":"4"
+  }
+]}`;
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,38 @@
-document.getElementById("bardOption").addEventListener("click", function(){hiLog(document.getElementById("bardOption").value)});
-document.getElementById("clericOption").addEventListener("click", function(){hiLog(document.getElementById("clericOption").value)});
-document.getElementById("druidOption").addEventListener("click", function(){hiLog(document.getElementById("druidOption").value)});
+(function(){
+  //takes class names from JSON and populates dropdown
+  let obj = JSON.parse(dndClassJSON);
+  let classArray = obj.bard;
+  let l = classArray.length;
 
-document.getElementById("fighterOption").addEventListener("click", function(){hiLog(document.getElementById("fighterOption").value)});
-document.getElementById("paladinOption").addEventListener("click", function(){hiLog(document.getElementById("paladinOption").value)});
-document.getElementById("rangerOption").addEventListener("click", function(){hiLog(document.getElementById("rangerOption").value)});
-document.getElementById("rogueOption").addEventListener("click", function(){hiLog(document.getElementById("rogueOption").value)});
-document.getElementById("sorcererOption").addEventListener("click", function(){hiLog(document.getElementById("sorcererOption").value)});
-document.getElementById("warlockOption").addEventListener("click", function(){hiLog(document.getElementById("warlockOption").value)});
-document.getElementById("wizardOption").addEventListener("click", function(){hiLog(document.getElementById("wizardOption").value)});
-
-
-
-
+  for (let i = 0; i < l; i++) {
+    let selectSpace = document.getElementById("classList");
+    let option = document.createElement("OPTION");
+    let optionText = document.createTextNode(classArray[i].name);
+    option.appendChild(optionText);
+    option.setAttribute("value", classArray[i].name);
+    option.setAttribute("id", classArray[i].name + "Option");
+    selectSpace.appendChild(option);
+    document.getElementById(classArray[i].name + "Option")
+    .addEventListener("click", function(){hiLog(classArray[i].name)});
+  }
+}());
 
 function hiLog(word){
   console.log("hello " + word);
+  // readJSON(word);
 }
 
+function readJSON(word){
+  let obj = JSON.parse(bardJson);
+  let classArray = obj.bard;
+  console.log(classArray[0]);
 
-function myFunction() {
-  console.log("Hello");
+  let name = obj.bard[0].name;
+  let cantrips = obj.bard[0].cantrips;
+  let l1 = obj.bard[0].l1;
 
-//   let classValue = document.getElementById("classList").value;
-//
-//   let myTable= "<table><tr><td>Col Head 1</td>";
-//     myTable+= "<td>Col Head 2</td>";
-//     myTable+="<td>Col Head 3</td></tr>";
-//
-//     myTable+="<tr><td>---------------</td>";
-//     myTable+="<td>---------------</td>";
-//     myTable+="<td>---------------</td></tr>";
-//
-// // let obj = JSON.parse(bardJson);
-//
-// // console.log("Hello");
-//
-//
-//
-//   document.getElementById("dataSpace").innerHTML = "Num: " + myTable;
+  document.getElementById("dataSpace").innerHTML = "Name: " + name + "<br>"
+  + "Cantips: " + cantrips + "<br>"
+  + "Level 1: " + l1;
+  // console.log(obj);
 }
