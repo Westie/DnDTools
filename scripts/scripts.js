70a4554d4c4ebe29056af409daf267e0582ac4b5
Dallamore <25459576+Dallamore@users.noreply.github.com>
2019-01-04 17:45:34 +0000

    Completed classes, added JSON object to table row

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,106 @@
-(function(){
-  //takes class names from JSON and populates dropdown
-  let obj = JSON.parse(dndClassJSON);
-  let classArray = obj.bard;
-  let l = classArray.length;
+let obj = JSON.parse(dndClassJSON);
+let fullClassArray = obj.classDataArrays;
+let l = fullClassArray.length;
 
+(function(){
+  //takes dnd class names from JSON and populates dropdown
   for (let i = 0; i < l; i++) {
     let selectSpace = document.getElementById("classList");
     let option = document.createElement("OPTION");
-    let optionText = document.createTextNode(classArray[i].name);
+    let optionText = document.createTextNode(fullClassArray[i].name);
     option.appendChild(optionText);
-    option.setAttribute("value", classArray[i].name);
-    option.setAttribute("id", classArray[i].name + "Option");
+    option.setAttribute("value", fullClassArray[i].name);
+    option.setAttribute("id", fullClassArray[i].name + "Option");
     selectSpace.appendChild(option);
-    document.getElementById(classArray[i].name + "Option")
-    .addEventListener("click", function(){hiLog(classArray[i].name)});
+    document.getElementById(fullClassArray[i].name + "Option")
+      .addEventListener("click", function(){addToPage(fullClassArray[i])});
   }
 }());
 
-function hiLog(word){
-  console.log("hello " + word);
-  // readJSON(word);
+function addToPage(singleClassArray){
+  // console.log(singleClassArray);
+  if(document.getElementById("activeTable") != null){
+    document.getElementById("dataSpace").replaceChild(tableMaker(singleClassArray), document.getElementById("activeTable"));
+  }else{
+    document.getElementById("dataSpace").appendChild(tableMaker(singleClassArray))
+  }
 }
 
-function readJSON(word){
-  let obj = JSON.parse(bardJson);
-  let classArray = obj.bard;
-  console.log(classArray[0]);
+function tableMaker(singleClassArray){
+  let tableOuter = document.createElement("TABLE");
 
-  let name = obj.bard[0].name;
-  let cantrips = obj.bard[0].cantrips;
-  let l1 = obj.bard[0].l1;
 
-  document.getElementById("dataSpace").innerHTML = "Name: " + name + "<br>"
-  + "Cantips: " + cantrips + "<br>"
-  + "Level 1: " + l1;
-  // console.log(obj);
+  for (var key in singleClassArray) {
+    if (singleClassArray.hasOwnProperty(key)) {
+        let tableRow = document.createElement("TR");
+        let rowContent = document.createTextNode(key + " -> " + singleClassArray[key]);
+        console.log(singleClassArray);
+        tableRow.appendChild(rowContent);
+        tableOuter.appendChild(tableRow);
+    }
 }
+
+
+  // for (let i = 0; i < Object.keys(singleClassArray).length; i++) {
+  //   let tableRow = document.createElement("TR");
+  //   // let rowContent = document.createTextNode(singleClassArray[i]);
+  //   console.log(singleClassArray);
+  //   tableRow.appendChild(rowContent);
+  //   tableOuter.appendChild(tableRow);
+  //
+  // }
+
+
+  // let tableCell = document.createElement("TD");
+
+
+
+  // let tableCellText = document.createTextNode(JSON.stringify(singleClassArray));
+  // tableCell.appendChild(tableCellText);
+  // tableRow.appendChild(tableCell);
+
+  tableOuter.setAttribute("id", "activeTable");
+
+
+
+  return tableOuter;
+}
+
+
+
+
+
+
+
+// <table>
+//   <tr>
+//     <td>Col Head 1</td>
+//     <td>Col Head 2</td>
+//     <td>Col Head 3</td>
+//   </tr>
+//   <tr>
+//     <td>normal cell 1</td>
+//     <td>normal cell 2</td>
+//     <td>normal cell 3</td>
+//   </tr>
+// </table>
+
+
+
+
+
+
+
+
+// function readJSON(word){
+//   console.log(fullClassArray[0]);
+//
+//   let name = obj.classDataArrays[0].name;
+//   let cantrips = obj.classDataArrays[0].cantrips;
+//   let l1 = obj.classDataArrays[0].l1;
+//
+//   document.getElementById("dataSpace").innerHTML = "Name: " + name + "<br>"
+//   + "Cantips: " + cantrips + "<br>"
+//   + "Level 1: " + l1;
+//   // console.log(obj);
+// }
