f7e429653b68e7cb02f6c2bfb79e6cc4ed7bf4a0
Dallamore <25459576+Dallamore@users.noreply.github.com>
2019-01-22 13:00:25 +0000

    Dynamically generated table Y label

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ let fullClassArray = obj.classDataArrays;
     option.setAttribute("id", fullClassArray[i].name + "Option");
     selectSpace.appendChild(option);
     document.getElementById(fullClassArray[i].name + "Option")
-      .addEventListener("click", function(){addToPage(fullClassArray[i])});
+    .addEventListener("click", function(){addToPage(fullClassArray[i])});
   }
 }());
 
@@ -30,59 +30,54 @@ function addToPage(singleClassArray){
 }
 
 function tableMaker(singleClassArray){
-//creates table to be added to page
+  //creates table to be added to page
   let tableOuter = document.createElement("TABLE");
-  for (var key in singleClassArray) {
+
+  let keyArray = Object.keys(singleClassArray);
+  let count = 1;
+  // console.log(keyArray);
+
+  for (let key in singleClassArray) {
+    let tableRow = document.createElement("TR");
     //loop through keys in class data
-    if (singleClassArray.hasOwnProperty(key)) {
-      let tableRow = document.createElement("TR");
-      for (let k = 0; k < singleClassArray.L20.length; k++){
-        //creates table cells for each stat + enough to make square table
+    if (!tableRow.innerHTML) {
+      if (key != "name") {
         let tableCell = document.createElement("TD");
-        if (typeof(singleClassArray[key][k]) == "undefined") {
-          //if cell is going to be empty
-          singleClassArray[key][k] = "";
-        }
-        if (key == 'name') {
-          //skip name of the class being seperated into individual letters per cell
-          continue;
-        }
-        if (singleClassArray[key][k] == "" || singleClassArray[key][k] == "-") {
-          //only creates an input box if value is a number
-          let cellContent = document.createTextNode(singleClassArray[key][k]);
-          tableCell.appendChild(cellContent);
-        }
-        else {
-          let cellContent = document.createElement("INPUT");
-          cellContent.setAttribute("type", "number");
-          cellContent.setAttribute("value", singleClassArray[key][k]);
-          cellContent.setAttribute("autocomplete", "off");
-          tableCell.appendChild(cellContent);
-        }
+        tableCell.setAttribute("class", "label");
+        let labelContent = document.createTextNode(keyArray[count]);
+        tableCell.appendChild(labelContent);
         tableRow.appendChild(tableCell);
+        count++;
       }
-      tableOuter.appendChild(tableRow);
     }
+
+    for (let k = 0; k < singleClassArray.L20.length; k++){
+      //creates table cells for each stat + enough to make square table
+      let tableCell = document.createElement("TD");
+      if (typeof(singleClassArray[key][k]) == "undefined") {
+        //if cell is going to be empty
+        singleClassArray[key][k] = "";
+      }
+      if (key == "name") {
+        //skip name of the class being seperated into individual letters per cell
+        continue;
+      }
+      if (singleClassArray[key][k] == "" || singleClassArray[key][k] == "-") {
+        //only creates an input box if value is a number
+        let cellContent = document.createTextNode(singleClassArray[key][k]);
+        tableCell.appendChild(cellContent);
+      }
+      else {
+        let cellContent = document.createElement("INPUT");
+        cellContent.setAttribute("type", "number");
+        cellContent.setAttribute("value", singleClassArray[key][k]);
+        cellContent.setAttribute("autocomplete", "off");
+        tableCell.appendChild(cellContent);
+      }
+      tableRow.appendChild(tableCell);
+    }
+    tableOuter.appendChild(tableRow);
   }
   tableOuter.setAttribute("id", "activeTable");
   return tableOuter;
 }
-
-// for (var key in singleClassArray) {
-//   if (singleClassArray.hasOwnProperty(key)) {
-//       let tableRow = document.createElement("TR");
-//       let rowContent = document.createTextNode(key + " -> " + singleClassArray[key]);
-//       // console.log(singleClassArray);
-//       tableRow.appendChild(rowContent);
-//       tableOuter.appendChild(tableRow);
-//   }
-// }
-
-  // for (let i = 0; i < Object.keys(singleClassArray).length; i++) {
-  //   let tableRow = document.createElement("TR");
-  //   // let rowContent = document.createTextNode(singleClassArray[i]);
-  //   console.log(singleClassArray);
-  //   tableRow.appendChild(rowContent);
-  //   tableOuter.appendChild(tableRow);
-  //
-  // }
