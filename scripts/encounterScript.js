db7cc015875c13b1a3b99ceef70bae52dbfc399e
Dallamore <25459576+Dallamore@users.noreply.github.com>
2019-01-28 12:47:45 +0000

    Prints selected monster's JSON obejct to the page in pre

--- scripts/encounterScript.js
+++ scripts/encounterScript.js
@@ -1,6 +1,6 @@
+let obj = JSON.parse(monstersJSON);
+let fullMonsterArray = obj.monsterDataArrays;
 (function(){
-  let obj = JSON.parse(monstersJSON);
-  let fullMonsterArray = obj.monsterDataArrays;
 
   let dropDown = document.getElementById("dropDownForMonsters");
   Array.from(fullMonsterArray).forEach(function(element){
@@ -9,4 +9,80 @@
     option.appendChild(optionText);
     dropDown.appendChild(option);
   })
+
+  let fightButton = document.getElementById("addToFightButton");
+  fightButton.onclick = function(){fightButtonListener();};
+
 }());
+
+function fightButtonListener(){
+  let fightBox = document.getElementById("fightBox");
+  let dropDown = document.getElementById("dropDownForMonsters");
+  let selectedMonsterIndex = dropDown.options[dropDown.selectedIndex].index - 1;
+  let selectedMonster = fullMonsterArray[selectedMonsterIndex];
+  // fightBox.appendChild(document.createTextNode(selectedMonster.name));
+
+  // let strJson = syntaxHighlight(selectedMonster);
+  // fightBox.appendChild(document.createTextNode(strJson));
+
+  var str = JSON.stringify(selectedMonster, undefined, 4);
+
+  // output(str);
+  document.body.appendChild(document.createElement('pre')).innerHTML = syntaxHighlight(str);
+
+}
+
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+
+// function LogJSONstringify(json) {
+//     if (typeof json != 'string') {
+//         json = JSON.stringify(json, undefined, '\t');
+//     }
+//     var arr = [],
+//         _string = 'color:green',
+//         _number = 'color:darkorange',
+//         _boolean = 'color:blue',
+//         _null = 'color:magenta',
+//         _key = 'color:red';
+//     json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+//         var style = _number;
+//         if (/^"/.test(match)) {
+//             if (/:$/.test(match)) {
+//                 style = _key;
+//             } else {
+//                 style = _string;
+//             }
+//         } else if (/true|false/.test(match)) {
+//             style = _boolean;
+//         } else if (/null/.test(match)) {
+//             style = _null;
+//         }
+//         arr.push(style);
+//         arr.push('');
+//         return '%c' + match + '%c';
+//     });
+//     arr.unshift(json);
+//     console.log.apply(console, arr);
+// }
